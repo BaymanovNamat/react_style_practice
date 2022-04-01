1e6cc89647f5f69716e1d2d1fcc7087622c45f77
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-01 13:36:39 +0600

    added User2 component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Table from "./components/Table/Table";
 import User from "./components/User/User";
+import User2 from "./components/User2/User2";
 
 function App() {
 
   return (
     <div className="App">
-      <Table/>
+
+      <div>
+        <Table/>
+      </div>
+      <br/> <br/> <br/>
 
       <div>
         <User/>
       </div>
 
+      <br/>
+      <br/>
+      <br/>
+
+      <div>
+        <User2/>
+      </div>
     </div>
   );
 }
--- src/components/User2/User2.js
+++ src/components/User2/User2.js
@@ -1,6 +1,75 @@
+import "./User2.css";
+
+
 function User2(props) {
+
+
+  const users = [
+    {
+      userId: 1,
+      id: 1,
+      age: 28,
+      name: "Namat",
+      title: "quidem molestiae enim",
+    },
+    {
+      userId: 1,
+      id: 1,
+      age: 38,
+      name: "Namat",
+      title: "quidem molestiae enim",
+    },
+    {
+      userId: 1,
+      id: 1,
+      age: 58,
+      name: "Namat",
+      title: "quidem molestiae enim",
+    },
+    {
+      userId: 5,
+      id: 2,
+      age: 78,
+      name: "Aziz",
+      title: "quidem molestiae enim",
+    },
+    {
+      userId: 2,
+      id: 5,
+      age: 27,
+      name: "Gulnaz",
+      title: "quidem molestiae enim",
+    },
+  ];
+
+
+  const userList = users.map((user) => (
+    <>
+      <tr className="line">
+        <td className="User2 modifygreen">
+          {user.userId}
+        </td>
+        <td className="User2 modifyred">
+          {user.id}
+        </td>
+        <td className="User2 modifyyellow">
+          {user.age}
+        </td>
+        <td className="User2 modifyviolet">
+          {user.name}
+        </td>
+        <td className="User2 modifyviocyan">
+          {user.title}
+        </td>
+      </tr>
+    </>
+  ));
+
+
   return (
-    <></>
+    <table className="User2">
+      {userList}
+    </table>
   );
 }
 
